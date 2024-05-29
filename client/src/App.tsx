897bde1b09d97e1e8368af77fee58ec1528a50d7
shumancheng <Shumancheng0119@gmail.com>
2024-05-29 22:01:03 +0200

    Rest Password Page Setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import ForgotPassword from "./screens/Auth/ForgotPassword";
 import LoginScreen from "./screens/Auth/LoginScreen";
+import ResetPassword from "./screens/Auth/ResetPassword";
 
 function App() {
   return (
     <div>
-      <ForgotPassword />
+      <ResetPassword />
     </div>
   );
 }
