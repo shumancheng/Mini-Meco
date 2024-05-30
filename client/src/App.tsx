5d7b897b36a1dafab2a452b71012dae59fa70be3
shumancheng <Shumancheng0119@gmail.com>
2024-05-30 08:53:50 +0200

    Route Setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import ForgotPassword from "./screens/Auth/ForgotPassword";
 import LoginScreen from "./screens/Auth/LoginScreen";
 import ResetPassword from "./screens/Auth/ResetPassword";
@@ -6,7 +7,14 @@ import ResetPassword from "./screens/Auth/ResetPassword";
 function App() {
   return (
     <div>
-      <ResetPassword />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="forgotPassword" element={<ForgotPassword />} />
+          <Route path="resetPassword" element={<ResetPassword />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
