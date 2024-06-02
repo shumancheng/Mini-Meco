fe1e98a67e6064fc508f037c36fee2a1e275cf1c
shumancheng <Shumancheng0119@gmail.com>
2024-06-02 21:39:18 +0200

    FinishPasswordReset

--- client/src/screens/Auth/ResetPassword.tsx
+++ client/src/screens/Auth/ResetPassword.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import "./LoginScreen.css";
 
-// Helper function to extract query parameters
+
 const useQuery = () => {
   return new URLSearchParams(useLocation().search);
 };
