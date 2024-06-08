7a36284b2d853a0203b765f61de600b416833b8d
shumancheng <Shumancheng0119@gmail.com>
2024-06-08 22:54:55 +0200

    ProjectAdmin UI

--- client/src/components/Administration/ProjectAdmin.tsx
+++ client/src/components/Administration/ProjectAdmin.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./ProjectAdmin.css";
+import {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectLabel,
+  SelectItem,
+  SelectSeparator,
+  SelectScrollUpButton,
+  SelectScrollDownButton,
+} from "@/components/ui/select";
+import Add from "./../../assets/Add.png";
 
 const ProjectAdmin: React.FC = () => {
   const navigate = useNavigate();
@@ -15,6 +28,10 @@ const ProjectAdmin: React.FC = () => {
     e.stopPropagation();
     navigate("/dashboard");
   };
+
+  const semesters = ["SS23", "WS2324", "SS24", "WS2425"];
+  const projectGroups = ["AMOS", "ADAP"];
+
   return (
     <div onClick={handleNavigation}>
       <div className="Return" onClick={returnToDashboard}>
@@ -27,11 +44,44 @@ const ProjectAdmin: React.FC = () => {
         <div className="ProjectGroupContainer">
           <div className="title">
             <h3>Project Group Lists</h3>
+            <Select>
+              <SelectTrigger className="SelectTrigger">
+                <SelectValue
+                  className="SelectValue"
+                  placeholder="Select Semester"
+                />
+              </SelectTrigger>
+              <SelectContent className="SelectContent">
+                {semesters.map((semester) => (
+                  <SelectItem key={semester} value={semester}>
+                    {semester}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="Add">
+            <img src={Add} />
           </div>
         </div>
         <div className="ProjectContainer">
           <div className="ProjectTitle">
             <h3>Project Lists</h3>
+            <Select>
+              <SelectTrigger className="SelectTrigger">
+                <SelectValue
+                  className="SelectValue"
+                  placeholder="Select Project Group"
+                />
+              </SelectTrigger>
+              <SelectContent className="SelectContent">
+                {projectGroups.map((group) => (
+                  <SelectItem key={group} value={group}>
+                    {group}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
           </div>
         </div>
       </div>
