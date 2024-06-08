21b6cc035a719a25935eace022a59cd17893e8db
shumancheng <Shumancheng0119@gmail.com>
2024-06-08 16:45:03 +0200

    SettingScreenUISetUp

--- client/src/components/Configuration/Settings.tsx
+++ client/src/components/Configuration/Settings.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Settings.css";
 
 const Settings: React.FC = () => {
   const navigate = useNavigate();
@@ -10,7 +11,21 @@ const Settings: React.FC = () => {
 
   return (
     <div onClick={handleNavigation}>
-      <h3>Settings</h3>
+      <div className="DashboardContainer">
+        <h1>Project Admin</h1>
+      </div>
+      <div className="BigContainer">
+        <div className="ProjectGroupContainer">
+          <div className="title">
+            <h3>Project Group Lists</h3>
+          </div>
+        </div>
+        <div className="ProjectContainer">
+          <div className="ProjectTitle">
+            <h3>Project Lists</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
