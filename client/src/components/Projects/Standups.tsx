016c7c3d524dea5bdb8b400b2370362d83934bab
shumancheng <Shumancheng0119@gmail.com>
2024-06-23 23:36:45 +0200

    setup standups screen

--- client/src/components/Projects/Standups.tsx
+++ client/src/components/Projects/Standups.tsx
@@ -1,17 +1,40 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ReturnButton from "../Components/return";
+import "./Standups.css";
+import Button from "react-bootstrap/esm/Button";
 
 const Standups: React.FC = () => {
   const navigate = useNavigate();
-  const handliestandups = () => {
+  const handleStandups = () => {
     navigate("/standups");
   };
 
   return (
-    <div onClick={handliestandups}>
+    <div onClick={handleStandups}>
       <ReturnButton />
-      <h3>Standups</h3>
+      <div className="DashboardContainerStandups">
+        <h1>Standup Emails</h1>
+      </div>
+      <div className="BigContainerStandups">
+        <div className="InputContainer">
+          <div className="Done">
+            <div className="DoneTitle">Done</div>
+            <input className="DoneContainer" type="text" />
+          </div>
+          <div className="Plans">
+            <div className="PlansTitle">Plans</div>
+            <input className="PlansContainer" type="text" />
+          </div>
+          <div className="Challenges">
+            <div className="ChallengesTitle">Challenges</div>
+            <input className="ChallengesContainer" type="text" />
+          </div>
+        </div>
+        <Button className="SendButton" type="submit">
+          Send Email
+        </Button>
+      </div>
     </div>
   );
 };
