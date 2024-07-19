0411afb5ed226c9a1f61c31e826b0a65b664875a
shumancheng <Shumancheng0119@gmail.com>
2024-07-19 15:56:55 +0200

    update happiness.tsx

--- server/dist/projConfig.js
+++ server/dist/projConfig.js
@@ -69,7 +69,8 @@ exports.addURL = addURL;
 const getURL = async (req, res, db) => {
     const { email, project } = req.query;
     try {
-        const url = await db.get(`SELECT url FROM user_projects WHERE userEmail = ? AND projectName = ?`, [email, project]);
+        const urlObj = await db.get(`SELECT url FROM user_projects WHERE userEmail = ? AND projectName = ?`, [email, project]);
+        const url = urlObj ? urlObj.url : null;
         res.status(200).json({ url });
     }
     catch (error) {
