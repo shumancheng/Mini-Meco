d9023a4b3c70507de0f4ab6f3919fa191a171ef4
shumancheng <Shumancheng0119@gmail.com>
2024-07-30 14:34:22 +0200

    connect git api

--- client/src/components/Projects/CodeActivity.tsx
+++ client/src/components/Projects/CodeActivity.tsx
@@ -1,40 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import ReturnButton from "../Components/return";
-import {Octokit} from "octokit";
+import { Octokit } from "@octokit/rest";
 
 const CodeActivity: React.FC = () => {
   const navigate = useNavigate();
 
+  const [issue, setIssue] = useState<any>(null);
   const handleNavigation = () => {
     navigate("/code-activity");
   };
 
-  const octokit = new Octokit({ 
-    auth: process.env.Token
+  const octokit = new Octokit({
+    auth: import.meta.env.VITE_GITHUB_TOKEN,
   });
 
-  const getIssue = async () =>{
+  const getIssue = async () => {
     try {
-      const response = await octokit.request("GET /repos/{owner}/{repo}/issues", {
-        owner: "octocat",
-        repo: "Spoon-Knife",
-      });
-      
-
-      console.log(response);
+      const response = await octokit.request(
+        "GET /repos/{owner}/{repo}/commits",
+        {
+          owner: "shumancheng",
+          repo: "Mini-Meco",
+        }
+      );
+      setIssue(response.data);
     } catch (error: any) {
-      console.error(`Error! Status: ${error.status}. Message: ${error.response.data.message}`);
+      console.error(
+        `Error! Status: ${error.status}. Message: ${error.response.data.message}`
+      );
     }
-  }
+  };
 
+  useEffect(() => {
+    getIssue();
+  }, []);
 
   return (
     <div onClick={handleNavigation}>
       <ReturnButton />
       <h3>Code Activity</h3>
-      <h3>Code Activity</h3>
-      <h3>Code Activity</h3>
+      {issue ? <pre>{JSON.stringify(issue, null, 2)}</pre> : <p>Loading...</p>}
     </div>
   );
 };
