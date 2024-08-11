fb00398a2284edc6fc6d1c90737b06d728560999
shumancheng <Shumancheng0119@gmail.com>
2024-08-11 10:31:02 +0200

    Confirm EMail UI

--- client/src/screens/Auth/ConfirmedEmail.tsx
+++ client/src/screens/Auth/ConfirmedEmail.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const ConfirmedEmail = () => {
-  return (
-    <div>ConfirmedEmail</div>
-  )
-}
+    const [message, setMessage] = useState("");
+
+    return (
+        <>
+          <div className="container">
+            <div className="COnfirmEMailheader">
+              <div className="text">Confirm Email</div>
+              <br />
+              <div className="underline"></div>
+            </div>
+            <div className="text ConfirmEmailText">
+            Thank you for confirming your email!
+              <br /> Please click the button to confirm and go back to Login Page
+            </div>
+              <div className="ConfirmEmailsubmit-container">
+                <button type="submit" className="submit">
+                  Confirm
+                </button>
+              </div>
+
+            {message && <div className="message">{message}</div>}
+          </div>
+        </>
+      );
+    };
 
 export default ConfirmedEmail
