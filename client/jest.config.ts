12868f9bd0f02776fe62580bc586ac5e0bb1b0ad
shumancheng <Shumancheng0119@gmail.com>
2024-09-01 10:54:01 +0200

    jest test run succeed

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -2,11 +2,21 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',  
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts'],
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+  testMatch: ['**/__tests__/**/*.(spec|test).ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.css$': 'jest-transform-stub',  // Handle CSS imports
+    '^.+\\.(jpg|jpeg|png|gif|svg|webp|avif)$': 'jest-transform-stub',  // Handle image imports
+  },
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',  
+    '\\.(gif|ttf|eot|svg|png)$': 'jest-transform-stub',
+  },
+  
 };
 
 export default config;
